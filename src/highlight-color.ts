37de2c163da126c35d5acddee9135d3fbc1b66dc
wangzhongxi <wangzhongxi@meituan.com>
2022-08-09 21:13:12 +0800

    feat: remove highlight color

--- src/highlight-color.ts
+++
@@ -1,4 +0,0 @@
-// import { Decoration, DecorationSet, EditorView } from "prosemirror-view";
-// import { EditorState, Plugin, PluginKey } from "prosemirror-state";
-
-// const colorReg = /#(?:[0-9a-fA-F]{6})/g;
